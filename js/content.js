8d8b2c9966cc6212100b29a055d76888100ea261
Peizhi Liu <peizhiliu168@gmail.com>
2020-09-09 02:08:47 -0400

    implementing <tabslist2html>

--- js/content.js
+++ js/content.js
@@ -2,7 +2,8 @@ console.log('content script running')
 
 chrome.runtime.onMessage.addListener((request, sender, send_response) => {
     if (request.message == 'toggle_tabs_window'){
-        // inject the html
+        var tabs_window = tabslist2html(request.data);
+        $(tabs_window).appendTo('body');
         send_response({message: request.message, return: 0});
     } else if (request.message == 'untoggle_tabs_window'){
         // remove the html
@@ -10,10 +11,20 @@ chrome.runtime.onMessage.addListener((request, sender, send_response) => {
     }
 })
 
-function tabstack2html(tabs_stack){
-    
+// a helper function for <inject_window()> that takes in 
+// a <tabs_stack> and convert that into html string that 
+// can be displayed
+function tabslist2html(tabs_list){
+    var tabs_window_template = $.get(chrome.runtime.getURL('templates/tabs_window.html'));
+    tabs_list.forEach((tab) => {
+        var tab_template = $.get(chrome.runtime.getURL('templates/tab_template.html'));
+        $(tab_template).find('.icon-capture').attr('src', tab.favIconUrl);
+        $(tab_template).find('.tab-description').text(tab.title);
+        $(tab_template).find('.tab-capture').attr('src', tab.preview_image_path);
+        $(tab_template).appendTo($(tabs_window_template).find('#tabs-holder'));
+    })
+    return tabs_window_template;
 }
-
 
 /*
     The content script is in charge of capturing the
